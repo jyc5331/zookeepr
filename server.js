b9c1be6c30fffbbad2dc4cbbef95be0161ddd692
jyc5331 <jcosgrovecoding@gmail.com>
2021-01-23 13:31:44 -0500

    module 1 complete and notated

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
   let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    personalityTraitsArray.forEach((trait) => {
+      filteredResults = filteredResults.filter(
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
   if (query.diet) {
     filteredResults = filteredResults.filter(
       (animal) => animal.diet === query.diet
@@ -25,14 +38,29 @@ function filterByQuery(query, animalsArray) {
   }
   return filteredResults;
 }
-
+//goes through the array and returns a single specified instance
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
 //get takes two arguments, the first establishes from whence, the second is a callback function with instructions
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+//this sends a 404 error if the application does not load properly
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 //this allows us to have a dynamically generated port that is Heroku friendly
