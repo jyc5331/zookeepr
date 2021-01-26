85f160377826ae20f7d861a5fe9056252f449b12
jyc5331 <jcosgrovecoding@gmail.com>
2021-01-25 19:24:08 -0500

    module 2 completed

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+//path is a node module that works with file and directory paths. It provides methods that assist working in 1/2
+//production environments like Heroku 2/2
+const path = require("path");
 const express = require("express");
 const { animals } = require("./data/animals");
 
@@ -5,6 +9,28 @@ const { animals } = require("./data/animals");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+//use allows us to run functions on the server that our requests will pass through, this creates middleware
+// parse incoming string or array data into key value pairs, extended true tells the method to search for subarrays inside of the returned data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -44,7 +70,23 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
+
+// add animal to json file and animals array in this function
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  //a synchronoous version of fs.writeFile that doesn't have a callback function, better for smaller databases
+  fs.writeFileSync(
+    //the following writes our animal array to animals/jason __dirname specifies where the information will go
+    path.join(__dirname, "./data/animals.json"),
+    //information must be saved as JSON!, null stops us from editing our existing data, 2 puts white spaces between vars for legibility
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
 //get takes two arguments, the first establishes from whence, the second is a callback function with instructions
+//add what is after the slash to your URL or it will not display in the browser
 
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -54,13 +96,26 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-//this sends a 404 error if the application does not load properly
+//this allows us to access an individual animal by id at localhost and sends a 404 error if the application does not load properly
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
   } else {
     res.send(404);
+  }
+});
+
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
 });
 
